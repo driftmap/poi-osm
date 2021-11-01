f4c256adf2967871bb914a257943d54ad5b990a7
maybemkl <mikael.brunila@gmail.com>
2021-11-01 14:06:30 +0200

    more scripts to clean osm extract and convert to geojson

---
+++ drop.sh
@@ -0,0 +1,1 @@
+osmconvert quebec.nodes.osm >quebec.poi.osm --drop-ways --drop-author --drop-relations --drop-versions
---
+++ togeojson.sh
@@ -0,0 +1,1 @@
+osmconvert quebec.nodes.osm —-drop-ways —-drop-author —-drop-relations —-drop-versions >quebec.poi.osm
