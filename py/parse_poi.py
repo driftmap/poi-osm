a8274bc88755951c9e1e2f187949b0c0f154ce26
maybemkl <mikael.brunila@gmail.com>
2022-05-24 18:26:28 -0400

    fix labeling missmatch

--- py/parse_poi.py
+++ py/parse_poi.py
@@ -2,9 +2,23 @@ import json
 import pickle
 import sys
 
-from geojson import FeatureCollection, dump
+from geojson import FeatureCollection
 
 file = sys.argv[1]
+
+def if_attribute_in_properties(properties):
+    attr_labels = []
+    for attr in properties:
+        if attr == 'name':
+            attr_parts = properties[attr].split(" ")
+            for part in attr_parts:
+                attr_labels.append((part, attr.upper()))
+        if 'addr' in attr:
+            attr_split = attr.split(":")[1]
+            attr_parts = properties[attr].split(" ")
+            for part in attr_parts:
+                attr_labels.append((part, attr_split.upper()))
+    return attr_labels
 
 data = json.load(open(file,'r'))
 fc = FeatureCollection(data)
@@ -13,30 +27,7 @@ with_labels = 0
 with_coordinates = 0
 with_both = 0
 for idx, feat in enumerate(fc['features']):
-    label_data = []
-    if 'name' in feat['properties']:
-        name_parts = feat['properties']['name'].split(" ")
-        for part in name_parts:
-            label_data.append((part, 'NAME'))
-    if 'other_tags' in feat['properties']:
-        tags = feat['properties']['other_tags'].split('","')
-        for tag in tags:
-            key, value = tag.split("=>")
-            key = key.replace('"', '')
-            value = value.replace('"', '')
-            if 'addr' in key:
-                key = key.split(":")[1]
-                fc['features'][idx]['properties'][key] = value
-                if len(value) > 1:
-                    value_parts = value.split(" ")
-                    for part in value_parts:
-                        label_data.append((part, key.upper()))
-                else:
-                    label_data.append((value, key.upper()))
-            else:
-                fc['features'][idx]['properties']['other_tags'] = {}
-                fc['features'][idx]['properties']['other_tags'][key] = value
-
+    label_data = if_attribute_in_properties(feat['properties'])
     coordinates = feat['geometry']['coordinates']
 
     if len(label_data) > 0:
@@ -50,4 +41,4 @@ for idx, feat in enumerate(fc['features']):
 print(f"POIs with labels: {with_labels}")
 print(f"POIs with coordinates: {with_coordinates}")
 print(f"POIs with both: {with_both}")
-pickle.dump(labels,open('outputs/quebec.osm.text.tags.coords.pkl', 'wb'))
+pickle.dump(labels,open('outputs/can.quebec.osm.tagged.pkl', 'wb'))
