8cf2133ba7c78c1ca0dc880c8b1683b03cd2f8d8
maybemkl <mikael.brunila@gmail.com>
2022-05-24 15:47:26 -0400

    get coordinates, add diagnostics

--- clean_poi.py
+++ clean_poi.py
@@ -13,6 +13,9 @@ out_labels = 'quebec.osm.tags.txt'
 #file_words = open(out_words, 'a')
 #file_labels = open(out_labels, 'a')
 labels = []
+with_labels = 0
+with_coordinates = 0
+with_both = 0
 for idx, feat in enumerate(fc['features']):
     label_data = []
     if 'name' in feat['properties']:
@@ -38,12 +41,22 @@ for idx, feat in enumerate(fc['features']):
                 fc['features'][idx]['properties']['other_tags'] = {}
                 fc['features'][idx]['properties']['other_tags'][key] = value
 
+    coordinates = feat['geometry']['coordinates']
+
     if len(label_data) > 0:
+        with_labels += 1
         #file_words.write(f"{','.join(words)}\n")
         #file_labels.write(f"{','.join(label_data)}\n")
-        print(label_data)
-        labels.append(label_data)
-pickle.dump(labels,open('quebec.osm.tagged_test.pkl', 'wb'))
+        labels.append((label_data, coordinates))
+        if len(coordinates) > 0:
+            with_both += 1
+    if len(coordinates) > 0:
+        with_coordinates += 1
+
+print(f"With labels: {with_labels}")
+print(f"With coordinates: {with_coordinates}")
+print(f"With both: {with_both}")
+pickle.dump(labels,open('quebec.osm.text.tags.coords.pkl', 'wb'))
 
 #with open(f'{file}_cleaned.geojson', 'w') as f:
 #    dump(fc, f)
