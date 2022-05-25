bbeabc17876689c48d84079e0988ebc5c2f3c9c7
maybemkl <mikael.brunila@gmail.com>
2022-05-24 22:09:11 -0400

    add other features

--- py/parse_poi.py
+++ py/parse_poi.py
@@ -1,24 +1,34 @@
 import json
 import pickle
+import re
 import sys
 
 from geojson import FeatureCollection
 
 file = sys.argv[1]
 
+address_tags = [
+    'name','city','street','postcode','housenumber','suburb','unit','housename'
+]
+
 def if_attribute_in_properties(properties):
     attr_labels = []
+    other = {}
     for attr in properties:
-        if attr == 'name':
+        if attr in ['name']:
             attr_parts = properties[attr].split(" ")
             for part in attr_parts:
                 attr_labels.append((part, attr.upper()))
-        if 'addr' in attr:
+        elif 'addr' in attr:
             attr_split = attr.split(":")[1]
-            attr_parts = properties[attr].split(" ")
-            for part in attr_parts:
-                attr_labels.append((part, attr_split.upper()))
-    return attr_labels
+            if attr_split in address_tags:
+                attr_parts = properties[attr].split(" ")
+                for part in attr_parts:
+                    attr_labels.append((part, attr_split.upper()))
+        else:
+            if attr != 'id':
+                other[attr] = properties[attr]
+    return attr_labels, other
 
 data = json.load(open(file,'r'))
 fc = FeatureCollection(data)
@@ -27,12 +37,12 @@ with_labels = 0
 with_coordinates = 0
 with_both = 0
 for idx, feat in enumerate(fc['features']):
-    label_data = if_attribute_in_properties(feat['properties'])
+    label_data, other = if_attribute_in_properties(feat['properties'])
     coordinates = feat['geometry']['coordinates']
-
+    id = re.findall('\d+', feat['id'])
     if len(label_data) > 0:
         with_labels += 1
-        labels.append((label_data, coordinates))
+        labels.append((label_data, coordinates, id, other))
         if len(coordinates) > 0:
             with_both += 1
     if len(coordinates) > 0:
