648c812215d5477fd35cc8f9540ba5bf360dfdec
Mikey <mikey@Stalgias-MacBook-Pro.local>
2022-05-25 01:08:56 -0400

    got a bit more rigorous on the filtering

--- osm_pbf_to_nodes_osm.sh
+++ osm_pbf_to_nodes_osm.sh
@@ -2,17 +2,23 @@
 
 osmosis --read-pbf data/quebec.osm.pbf \
         --tf accept-nodes \
-        aerialway=station \
-        aeroway=aerodrome,helipad,heliport \
-        amenity=* building=school,university craft=* \
-        emergency=* \
-        highway=bus_stop,rest_area,services \
-        historic=* leisure=* office=* \
-        public_transport=stop_position,stop_area railway=station \
-        shop=* tourism=* \
-        addr:housenumber=* \
-        addr:street=* \
-        addr:postcode=* \
+         building=* \
+         aerialway=station \
+         aeroway=aerodrome,helipad,heliport \
+         amenity=* \
+         building=school,university craft=* \
+         highway=bus_stop,rest_area,services \
+         historic=* leisure=* office=* \
+         public_transport=stop_position,stop_area railway=station \
+         shop=* tourism=* \
+         addr:housenumber=* \
+         addr:street=* \
+         addr:postcode=* \
+         addr:city=* \
+        --tf reject-nodes \
+          amenity=drinking_water,watering_point,shower,telephone,toilets,bench,parcel_locker,bbq,dog_toilet,give_box,post_box,fountain \
+        --tf reject-ways \
+        --tf reject-relations \
         --write-xml quebec.nodes.osm
 # Test if the script above success
 if [ $? -eq 0 ]; then
