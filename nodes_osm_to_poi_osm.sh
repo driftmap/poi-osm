c3f144c0145c07d6f2e8af9a5331065f639b0031
Mikey <mikey@Stalgias-MacBook-Pro.local>
2022-05-25 00:27:15 -0400

    altered shell scripts and added addresses to an osm processor

--- nodes_osm_to_poi_osm.sh
+++ nodes_osm_to_poi_osm.sh
@@ -1,1 +1,6 @@
-osmconvert quebec.nodes.osm --drop-ways --drop-author --drop-relations --drop-version -o=quebec.poi.osm
+osmconvert quebec.nodes.osm \
+  --drop-ways \
+  --drop-author \
+  --drop-relations \
+  --drop-version \
+  -o=quebec.poi.osm
--- osm_pbf_to_nodes_osm.sh
+++ osm_pbf_to_nodes_osm.sh
@@ -1,10 +1,22 @@
+#!/bin/bash
+
 osmosis --read-pbf data/quebec.osm.pbf \
         --tf accept-nodes \
         aerialway=station \
         aeroway=aerodrome,helipad,heliport \
-        amenity=* building=school,university craft=* emergency=* \
+        amenity=* building=school,university craft=* \
+        emergency=* \
         highway=bus_stop,rest_area,services \
         historic=* leisure=* office=* \
         public_transport=stop_position,stop_area railway=station \
         shop=* tourism=* \
+        addr:housenumber=* \
+        addr:street=* \
+        addr:postcode=* \
         --write-xml quebec.nodes.osm
+# Test if the script above success
+if [ $? -eq 0 ]; then
+   echo 🥳 Successful script
+else
+   echo "❌ Script failed"
+fi
